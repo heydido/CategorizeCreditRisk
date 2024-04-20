bff54044f04582030edfc6df89b012d1bd2ece31
heydido <aashish4.iitd@gmail.com>
2024-04-20 16:56:15 +0530

    Added contants

--- src/CategorizeCreditRisk/constants/__init__.py
+++ src/CategorizeCreditRisk/constants/__init__.py
@@ -1,3 +1,8 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+RAW_SCHEMA_FILE_PATH = Path("raw_schema.yaml")
+PROCESSED_SCHEMA_FILE_PATH = Path("processed_schema.yaml")
